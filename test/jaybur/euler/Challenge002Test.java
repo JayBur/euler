112810c3ef88aada26fbed4d11d2d751b0808480
JayBur <jhartveldt@gmail.com>
2015-03-27 20:36:55 +0100

    c002 completed

---
+++ test/jaybur/euler/Challenge002Test.java
@@ -0,0 +1,19 @@
+package jaybur.euler;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Challenge002Test {
+
+	@Test
+	public void test_getSumOfEvenFibonacciNumbers() {
+		int result = Challenge002.getSumOfEvenFibonacciNumbers(100);
+		final int expectedResult = 44;
+
+		assertEquals(
+				"Result does not match up against hand-calculated result from example!",
+				expectedResult, result);
+	}
+
+}
